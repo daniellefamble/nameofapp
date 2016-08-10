791360b28734130881921139829050ab357d6007
Danielle Famble <daniellefamble@gmail.com>
2016-08-09 21:39:00 -0400

    5.3 updated sendgrid info and modified gemfile. Added dotenv

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 ruby '2.2.4'
 
+gem 'dotenv-rails', :groups => [:development, :test]
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
 # Use sqlite3 as the database for Active Record
--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
-require "rails"
+require "rails/all"
 # Pick the frameworks you want:
+require 'dotenv' ; Dotenv.load
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
