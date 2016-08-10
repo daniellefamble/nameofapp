04811eb771cf61e581169a29bd4d2826a9a75208
Danielle Famble <daniellefamble@gmail.com>
2016-08-10 17:12:54 -0400

    5.3 modified syntax in application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require "rails/all"
 # Pick the frameworks you want:
-require 'dotenv' ; Dotenv.load
+require 'dotenv'
+Dotenv.load
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
